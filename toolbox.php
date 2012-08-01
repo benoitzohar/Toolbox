deb62d7ab7f11c3257afca98a4fe5e8e5f761d20
Benoit Zohar <benoit.zohar@gmail.com>
2012-08-01 09:32:30 +0200

    Log displaying (and other stuff)

--- toolbox.php
+++ toolbox.php
@@ -36,13 +36,28 @@ class SocketClient {
 	}
 }
 
+function json_exit($status = false, $params = false) {
+	$array_to_jsonize = array('status' => $status);
+	if ($params && is_array($params)) {
+		foreach ($params as $key => $param) $array_to_jsonize[$key] = $param;
+	} else if ($params && is_string($params)) {
+		foreach (explode(',', $params) as $param) {
+			list($key, $value) = explode(':', $param);
+			$array_to_jsonize[$key] = $value;
+		}
+	}
+	echo json_encode($array_to_jsonize);
+	exit;
+}
+
 
 
 /*********** ACTIONS **************/
 
-if (!empty($_REQUEST['action'])) {
+$v = $_REQUEST;
+if (!empty($v['action'])) {
 	
-	switch ($_REQUEST['action']) {
+	switch ($v['action']) {
 		
 		case 'restart_apache' :
 			$ot = shell_exec('echo "sudo /etc/init.d/apache2 restart" | sudo at now');
@@ -54,6 +69,55 @@ if (!empty($_REQUEST['action'])) {
 			$ot = shell_exec('echo "sudo /etc/init.d/dsockbackoffice restart" | sudo at now');
 			echo 'The socket server is restarting ... '.$ot;
 			exit();
+		break;
+		
+		case 'get_log' : 
+			$limit = 50;
+			if (!empty($v['limit'])) $limit = intval($v['limit']);
+			if ($v['type'] == 'file') {
+				if (empty($v['filename'])) json_exit(false,'message:Please specify a filename');
+				$res = array(); $c = 0;
+				if (!$h= @fopen($v['filename'], "r")) json_exit(false,'message:Unable to read file');
+				while (($b=fgets($h))!==false) {
+					$c++;
+					if ($c > $limit) break;
+					array_push($res,$b);
+				}
+				 
+				fclose($h);
+				
+				json_exit(true,array('rows' => array_reverse($res)));
+			}
+			else if ($v['type'] == 'db') {
+				if (empty($v['field'])) json_exit(false,'message:Please specify content fieldname');
+				if (empty($v['table'])) json_exit(false,'message:Please specify table name');
+				$db_conf = array();
+				$db = false;
+				if (!empty($v['db'])) $db = $v['db'];//@json_decode($v['db'],true);
+			
+				foreach(array('host','dbname','user','pass') as $k) {
+					$db_conf[$k] = $_SESSION['toolbox_'.$k];
+					if (is_array($db) && !empty($db[$k])) $db_conf[$k] = $db[$k];
+					if (empty($db_conf[$k])) json_exit(false,'message:missing param '.$k);
+					$_SESSION['toolbox_'.$k] = $db_conf[$k];
+					
+				}
+				try { 
+					$pdo = new PDO('mysql:host='.$db_conf['host'].';dbname='.$db_conf['dbname'],$db_conf['user'],$db_conf['pass']);
+					$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				} catch(Exception $e) { json_exit(false,'message: could not connect to DB'); }
+				$filter = '';				
+				if (!empty($v['filter_col']) && !empty($v['filter_val'])) $filter = 'WHERE '.$v['filter_col']." = '".$v['filter_val']."' ";
+				if (!empty($v['filter_sort'])) $filter .= " ORDER BY ".$v['filter_sort']." DESC";
+				$r = $pdo->query("SELECT ".$v['field']." FROM ".$v['table']." ".$filter." LIMIT 0,".$limit);
+				$res = array();
+				while ($row = $r->fetch(PDO::FETCH_ASSOC)) {
+					if (!empty($v['last_row']) && $row[$v['field']] == $v['last_row']) break;
+					array_push($res,$row[$v['field']]);
+				}
+				json_exit(true,array('rows' => array_reverse($res)));
+			}
+			json_exit(false,'message:No log type precised');
 		break;
 		
 	}
@@ -223,10 +287,9 @@ if (!empty($_REQUEST['action'])) {
 	      </div>
 	    </div>
 	    <div class="tab-pane" id="tab2" style="padding-left:5px;">
-		    	<div class="input-prepend"><span class="add-on">host</span><input id="log_config_db_host" type="text" class="span2" /></div><br />
-		    	<div class="input-prepend"><span class="add-on">user</span><input id="log_config_db_user" type="text" class="span2" /></div><br />
-		    	<div class="input-prepend"><span class="add-on">password</span><input id="log_config_db_password" type="password" class="span2" /></div><br />
+		    	<div class="input-prepend"><span class="add-on">host</span><input id="log_config_db_host" type="text" class="span2" /><span class="add-on">user</span><input id="log_config_db_user" type="text" class="span2" /><span class="add-on">password</span><input id="log_config_db_password" type="password" class="span2" /></div><br />
 		    	<div class="input-prepend"><span class="add-on">database</span><input id="log_config_db_database" type="text" class="span2" /></div><br />
+		    	<div class="input-prepend"><span class="add-on">table</span><input id="log_config_db_table" type="text" class="span2" /></div><br />
 		    	<div class="input-prepend"><span class="add-on">content fieldname</span><input id="log_config_db_fieldname" type="text" class="span2" /></div><br />
 		    	<div class="input-prepend">
 		    		<span class="add-on">filter</span>
@@ -253,7 +316,7 @@ if (!empty($_REQUEST['action'])) {
 
 <script>
 var row_limit = 7;
-	var xhr_fields = ["xhr_url","log_config_filepath","log_config_db_host","log_config_db_user","log_config_db_password","log_config_db_database","log_config_db_fieldname"];
+	var xhr_fields = ["xhr_url","log_config_filepath","log_config_db_host","log_config_db_user","log_config_db_password","log_config_db_database","log_config_db_fieldname","log_config_db_table"];
 	for(var i=0;i<=row_limit;i++) {xhr_fields.push("xhr_key"+i);xhr_fields.push("xhr_val"+i);}
 	
 	function getPostDataXHR() {
@@ -338,7 +401,7 @@ var row_limit = 7;
 	
 	// md5
 	function array(a){for(i=0;i<a;i++)this[i]=0;this.length=a}function integer(a){return a%(4294967295+1)}function shr(a,b){a=integer(a);b=integer(b);if(a-2147483648>=0){a=a%2147483648;a>>=b;a+=1073741824>>b-1}else a>>=b;return a}function shl1(a){a=a%2147483648;if(a&1073741824==1073741824){a-=1073741824;a*=2;a+=2147483648}else a*=2;return a}function shl(a,b){a=integer(a);b=integer(b);for(var c=0;c<b;c++)a=shl1(a);return a}function and(a,b){a=integer(a);b=integer(b);var c=a-2147483648;var d=b-2147483648;if(c>=0)if(d>=0)return(c&d)+2147483648;else return c&b;else if(d>=0)return a&d;else return a&b}function or(a,b){a=integer(a);b=integer(b);var c=a-2147483648;var d=b-2147483648;if(c>=0)if(d>=0)return(c|d)+2147483648;else return(c|b)+2147483648;else if(d>=0)return(a|d)+2147483648;else return a|b}function xor(a,b){a=integer(a);b=integer(b);var c=a-2147483648;var d=b-2147483648;if(c>=0)if(d>=0)return c^d;else return(c^b)+2147483648;else if(d>=0)return(a^d)+2147483648;else return a^b}function not(a){a=integer(a);return 4294967295-a}function F(a,b,c){return or(and(a,b),and(not(a),c))}function G(a,b,c){return or(and(a,c),and(b,not(c)))}function H(a,b,c){return xor(xor(a,b),c)}function I(a,b,c){return xor(b,or(a,not(c)))}function rotateLeft(a,b){return or(shl(a,b),shr(a,32-b))}function FF(a,b,c,d,e,f,g){a=a+F(b,c,d)+e+g;a=rotateLeft(a,f);a=a+b;return a}function GG(a,b,c,d,e,f,g){a=a+G(b,c,d)+e+g;a=rotateLeft(a,f);a=a+b;return a}function HH(a,b,c,d,e,f,g){a=a+H(b,c,d)+e+g;a=rotateLeft(a,f);a=a+b;return a}function II(a,b,c,d,e,f,g){a=a+I(b,c,d)+e+g;a=rotateLeft(a,f);a=a+b;return a}function transform(a,b){var c=0,d=0,e=0,f=0;var g=transformBuffer;c=state[0];d=state[1];e=state[2];f=state[3];for(i=0;i<16;i++){g[i]=and(a[i*4+b],255);for(j=1;j<4;j++){g[i]+=shl(and(a[i*4+j+b],255),j*8)}}c=FF(c,d,e,f,g[0],S11,3614090360);f=FF(f,c,d,e,g[1],S12,3905402710);e=FF(e,f,c,d,g[2],S13,606105819);d=FF(d,e,f,c,g[3],S14,3250441966);c=FF(c,d,e,f,g[4],S11,4118548399);f=FF(f,c,d,e,g[5],S12,1200080426);e=FF(e,f,c,d,g[6],S13,2821735955);d=FF(d,e,f,c,g[7],S14,4249261313);c=FF(c,d,e,f,g[8],S11,1770035416);f=FF(f,c,d,e,g[9],S12,2336552879);e=FF(e,f,c,d,g[10],S13,4294925233);d=FF(d,e,f,c,g[11],S14,2304563134);c=FF(c,d,e,f,g[12],S11,1804603682);f=FF(f,c,d,e,g[13],S12,4254626195);e=FF(e,f,c,d,g[14],S13,2792965006);d=FF(d,e,f,c,g[15],S14,1236535329);c=GG(c,d,e,f,g[1],S21,4129170786);f=GG(f,c,d,e,g[6],S22,3225465664);e=GG(e,f,c,d,g[11],S23,643717713);d=GG(d,e,f,c,g[0],S24,3921069994);c=GG(c,d,e,f,g[5],S21,3593408605);f=GG(f,c,d,e,g[10],S22,38016083);e=GG(e,f,c,d,g[15],S23,3634488961);d=GG(d,e,f,c,g[4],S24,3889429448);c=GG(c,d,e,f,g[9],S21,568446438);f=GG(f,c,d,e,g[14],S22,3275163606);e=GG(e,f,c,d,g[3],S23,4107603335);d=GG(d,e,f,c,g[8],S24,1163531501);c=GG(c,d,e,f,g[13],S21,2850285829);f=GG(f,c,d,e,g[2],S22,4243563512);e=GG(e,f,c,d,g[7],S23,1735328473);d=GG(d,e,f,c,g[12],S24,2368359562);c=HH(c,d,e,f,g[5],S31,4294588738);f=HH(f,c,d,e,g[8],S32,2272392833);e=HH(e,f,c,d,g[11],S33,1839030562);d=HH(d,e,f,c,g[14],S34,4259657740);c=HH(c,d,e,f,g[1],S31,2763975236);f=HH(f,c,d,e,g[4],S32,1272893353);e=HH(e,f,c,d,g[7],S33,4139469664);d=HH(d,e,f,c,g[10],S34,3200236656);c=HH(c,d,e,f,g[13],S31,681279174);f=HH(f,c,d,e,g[0],S32,3936430074);e=HH(e,f,c,d,g[3],S33,3572445317);d=HH(d,e,f,c,g[6],S34,76029189);c=HH(c,d,e,f,g[9],S31,3654602809);f=HH(f,c,d,e,g[12],S32,3873151461);e=HH(e,f,c,d,g[15],S33,530742520);d=HH(d,e,f,c,g[2],S34,3299628645);c=II(c,d,e,f,g[0],S41,4096336452);f=II(f,c,d,e,g[7],S42,1126891415);e=II(e,f,c,d,g[14],S43,2878612391);d=II(d,e,f,c,g[5],S44,4237533241);c=II(c,d,e,f,g[12],S41,1700485571);f=II(f,c,d,e,g[3],S42,2399980690);e=II(e,f,c,d,g[10],S43,4293915773);d=II(d,e,f,c,g[1],S44,2240044497);c=II(c,d,e,f,g[8],S41,1873313359);f=II(f,c,d,e,g[15],S42,4264355552);e=II(e,f,c,d,g[6],S43,2734768916);d=II(d,e,f,c,g[13],S44,1309151649);c=II(c,d,e,f,g[4],S41,4149444226);f=II(f,c,d,e,g[11],S42,3174756917);e=II(e,f,c,d,g[2],S43,718787259);d=II(d,e,f,c,g[9],S44,3951481745);state[0]+=c;state[1]+=d;state[2]+=e;state[3]+=f}function init(){count[0]=count[1]=0;state[0]=1732584193;state[1]=4023233417;state[2]=2562383102;state[3]=271733878;for(i=0;i<digestBits.length;i++)digestBits[i]=0}function update(a){var b,c;b=and(shr(count[0],3),63);if(count[0]<4294967295-7)count[0]+=8;else{count[1]++;count[0]-=4294967295+1;count[0]+=8}buffer[b]=and(a,255);if(b>=63){transform(buffer,0)}}function finish(){var a=new array(8);var b;var c=0,d=0,e=0;for(c=0;c<4;c++){a[c]=and(shr(count[0],c*8),255)}for(c=0;c<4;c++){a[c+4]=and(shr(count[1],c*8),255)}d=and(shr(count[0],3),63);e=d<56?56-d:120-d;b=new array(64);b[0]=128;for(c=0;c<e;c++)update(b[c]);for(c=0;c<8;c++)update(a[c]);for(c=0;c<4;c++){for(j=0;j<4;j++){digestBits[c*4+j]=and(shr(state[c],j*8),255)}}}function hexa(a){var b="0123456789abcdef";var c="";var d=a;for(hexa_i=0;hexa_i<8;hexa_i++){c=b.charAt(Math.abs(d)%16)+c;d=Math.floor(d/16)}return c}function MD5(a){var b,c,d,e,f,g,h;init();for(d=0;d<a.length;d++){b=a.charAt(d);update(ascii.lastIndexOf(b))}finish();e=f=g=h=0;for(i=0;i<4;i++)e+=shl(digestBits[15-i],i*8);for(i=4;i<8;i++)f+=shl(digestBits[15-i],(i-4)*8);for(i=8;i<12;i++)g+=shl(digestBits[15-i],(i-8)*8);for(i=12;i<16;i++)h+=shl(digestBits[15-i],(i-12)*8);c=hexa(h)+hexa(g)+hexa(f)+hexa(e);return c}var state=new array(4);var count=new array(2);count[0]=0;count[1]=0;var buffer=new array(64);var transformBuffer=new array(16);var digestBits=new array(16);var S11=7;var S12=12;var S13=17;var S14=22;var S21=5;var S22=9;var S23=14;var S24=20;var S31=4;var S32=11;var S33=16;var S34=23;var S41=6;var S42=10;var S43=15;var S44=21;var ascii="01234567890123456789012345678901"+" !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"+"[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-	
+	 //"
 	 
 	 function onEncrypt() {
 	 	var txta = $("#encrypt_txtarea");
@@ -401,13 +464,7 @@ var row_limit = 7;
 			bar.animate({height:height_small+'px'},300);
 		}
 	 }
-	 
-	 var log_data = [];
-	 function loadLog(restart) {
-	 	if (restart) log_data = [];
-	 	
-	 }
-	 
+
 	 var displ_counter = 0;
 	 var displ_counter_timer = false;
 	 function startCounter() {
@@ -425,6 +482,41 @@ var row_limit = 7;
 	 function stopCounter() {
 	 	if (displ_counter_timer) clearInterval(displ_counter_timer);
 	 }
+	 
+	 var log_data = [];
+	 var current_log_type = '';
+	 function doLog(restart) {
+	 	var ct = $('#log_content');
+	 	var data = {action:'get_log'};
+	 	if (current_log_type == 'db') {
+	 		data.type = 'db';
+	 		data.field = $('#log_config_db_fieldname').val();
+	 		data.table = $('#log_config_db_table').val();
+	 	} else if (current_log_type == 'file') {
+		 	data.type = 'file';
+		 	data.filename = $('#log_config_filepath').val();
+	 	}
+	 	else return false;
+	 	if (restart) {
+		 	log_data = [];
+		 	ct.empty();
+		 	data.db = {host:$('#log_config_db_host').val(),dbname:$('#log_config_db_database').val(),user:$('#log_config_db_user').val(),pass:$('#log_config_db_password').val()};
+	 	}
+	 	else {
+		 	data.last_row = log_data[log_data.length-1];
+	 	}
+	 	$.ajax("<?php echo $_SERVER["SCRIPT_NAME"]; ?>", {data:data,dataType: 'json',type:'POST',
+	 		success : function(d) {
+	 			if (!d) return ct.text("Unknonw error");
+	 			if (!d.status) return ct.text(d.message);
+	 			if (!d.rows || d.rows.length == 0) return ct.text("log is empty");
+	 			for(var i=0;i<d.rows.length;i++) {
+		 			log_data.push(d.rows[i]);
+		 			ct.append($('<div />').text(d.rows[i]));
+	 			}
+	 		}
+	 	});
+	 }
  
 	$(function() {
 	
@@ -433,7 +525,7 @@ var row_limit = 7;
 			if (/#/.test($(this).attr("href"))) {
 				$(this).click(function() {
 					setTimeout(function() {
-						$(".tabcont").addClass("hidden"); console.log(document.location.hash.replace("#",""));
+						$(".tabcont").addClass("hidden");
 						$(document.location.hash).removeClass("hidden");
 						$("div.navbar li").removeClass("active");
 						$("div.navbar a[href=\""+document.location.hash+"\"]").parents("li").addClass("active");
@@ -450,7 +542,7 @@ var row_limit = 7;
 		}
 			
 		if (localStorage) {
-			for(var k in xhr_fields){ console.log('k',xhr_fields[k]);
+			for(var k in xhr_fields){
 				if (localStorage[xhr_fields[k]]) $("#"+xhr_fields[k]).val(localStorage[xhr_fields[k]]);
 				$("#"+xhr_fields[k]).change(function(){
 					localStorage[$(this).attr("id")] = $(this).val();
@@ -460,7 +552,19 @@ var row_limit = 7;
 			
 			if (localStorage["restart_apache"]) $("#restart_apache .last_done span").text(localStorage["restart_apache"]);
 			if (localStorage["restart_socket"]) $("#restart_socket .last_done span").text(localStorage["restart_socket"]);
+			
+			if (localStorage['current_log_type']) {
+				current_log_type = localStorage['current_log_type'];
+				if (current_log_type=='file') $('#log_config_modal .nav-tabs a[href=#tab1]').click();
+				else if (current_log_type=='db') $('#log_config_modal .nav-tabs a[href=#tab2]').click();
+			}
 		}
+		
+		$('#log_config_modal .nav-tabs a').click(function(){ console.log(this.href);
+			current_log_type = ($(this).attr('href')=='#tab1' ? 'file' : 'db');
+			localStorage['current_log_type'] = current_log_type;
+		});
+		$('#log_config_modal').on('hide',function() { doLog(true);});
 	});
 
 </script>
